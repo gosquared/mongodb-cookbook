b507b946b569cbaeeeeafd5107219777f2fac58f
Geoff Wagstaff <geoffwagstaff@gmail.com>
2013-06-12 17:14:59 +0100

    Use default mongo dir

--- recipes/logrotate.rb
+++ recipes/logrotate.rb
@@ -3,7 +3,7 @@ include_recipe "bootstrap"
 # run manually: logrotate -vf /etc/logrotate.d/mongodb
 #
 logrotate "mongodb" do
-  paths "#{node[:mongodb][:logpath]}/*.log"
+  paths "#{node[:mongodb][:mongod][:logpath]}/*.log"
   period node[:mongodb][:logrotate][:period]
   keep node[:mongodb][:logrotate][:keep]
 end
